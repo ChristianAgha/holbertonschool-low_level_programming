9f02c5f5b2216243bbdbf46733279f352e336843
Christian Agha <christianagha@gmail.com>
2017-02-10 05:55:49 +0000

    remove exe files

---
+++ 0x01-variables_if_else_while/9-test.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num0, num1, num2, num3, num4, num5, num6, num7, num8, num9 = 48;
+	while (num9 <= 57)
+	{
+		while (num8 <= 56)
+		{
+			while (num7 <= 55}
+			{
+				while (num6 <= 54)
+				{
+					while (num5 <= 53)
+					{
+						while (num4 <= 52)
+						{
+							while (num3 <= 51)
+							{
+								while (num2 <= 50)
+								{
+									while (num1 <= 49)
+									{
+										while (num0 <= 48)
+										{
+										}
+									}
+
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		putchar(nine);
+		if (nine < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		nine++;
+	}
+	putchar('\n');
+	return (0);
+}
