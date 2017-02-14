b135dc67127600291ecd4a3f8869e4917465582d
Christian Agha <christianagha@gmail.com>
2017-02-14 22:51:09 +0000

    updates 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,21 +8,5 @@
  */
 int add(int a, int b)
 {
-	long r;
-	unsigned long int first;
-	unsigned long int second;
-	unsigned long int  u;
-
-	if (a >= 0 && b >= 0)
-	{
-		first = (unsigned long int) a;
-		second = (unsigned long int) b;
-		u = first + second;
-		return (u);
-	}
-	else
-	{
-		r = a + b;
-		return (r);
-	}
+	return (a + b);
 }
