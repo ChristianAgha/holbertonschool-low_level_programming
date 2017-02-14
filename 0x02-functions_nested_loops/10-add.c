8c4c1c455c6e32bbb73793d0e289542fffc0babf
Christian Agha <christianagha@gmail.com>
2017-02-14 20:32:22 +0000

    updated 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,12 @@
  * add - adds two integer and returns the result
  * @a: first number
  * @b: second number
+ * r: addition result
  * Return: result
  */
 int add(int a, int b)
 {
-	return (a + b);
+	int r = a + b;
+
+	return (r);
 }
