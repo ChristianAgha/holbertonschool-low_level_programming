b9f1ab4002a227bde08927c592186cf8fe101791
Christian Agha <christianagha@gmail.com>
2017-02-14 20:57:03 +0000

    updated 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,11 @@
 #include "holberton.h"
-
 /**
  * add - adds two integer and returns the result
- *
  * @a: first number
  * @b: second number
  *
  * Return: result
  */
-
 int add(int a, int b)
 {
 	unsigned int r;
