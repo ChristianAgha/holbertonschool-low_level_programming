b9061f879e2ae497665cb4067006c4a585fb2b41
Christian Agha <christianagha@gmail.com>
2017-02-15 07:52:18 +0000

    updated 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,21 +4,21 @@
  * main - prints the first 98 Fibonacci numbers
  * Return: 0
  */
+
 int main(void)
 {
-	unsigned long first = 1, second = 2, f, a;
+	long double first = 1, second = 2, a, f;
 
 	for (a = 1; a <= 96; a++)
 	{
 		if (a == 1)
 			printf("1, 2, ");
 		f = first + second;
-		printf("%lu", f);
-		if (a < 96)
+		printf("%1.0Lf", f);
+		if (a <= 95)
 			printf(", ");
 		first = second;
 		second = f;
-
 	}
 	printf("\n");
 	return (0);
