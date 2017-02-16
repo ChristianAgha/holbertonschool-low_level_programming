5c7018a401775a455e2a7d6cb485ffb8b2454e11
Christian Agha <christianagha@gmail.com>
2017-02-16 23:00:09 +0000

    added 2-mul.c

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *  mil - a function that multiplies two integers
+ * @a: int to be multiplied
+ * @b: int to be multiplied
+ * Return: 1 or 0
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
