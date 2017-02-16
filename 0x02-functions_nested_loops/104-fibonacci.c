12edad00cdf8a7fb7dce8c9154d4a342fe41aef4
Christian Agha <christianagha@gmail.com>
2017-02-16 00:05:14 +0000

    updated 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - prints the first 98 Fibonacci numbers
  * Return: 0
@@ -7,18 +6,41 @@
 
 int main(void)
 {
-	long double first = 1, second = 2, a, f;
+	unsigned long frp1, frp2, ar1, n1 = 1, n2 = 2;
+	int a, b, ar2, secp1, secp2;
 
 	for (a = 1; a <= 96; a++)
 	{
 		if (a == 1)
 			printf("1, 2, ");
-		f = first + second;
-		printf("%1.0Lf", f);
-		if (a <= 95)
+		if (a <= 90)
+		{
+			ar1 = (n1 + n2);
+			printf("%lu, ", ar1);
+			n1 = n2;
+			n2 = ar1;
+		}
+	}
+	frp1 = n1 / 100; frp2 = n2 / 100; secp1 = n1 % 100; secp2 = n2 % 100;
+	for (b = 1; b <= 6; b++)
+	{
+		ar1 = frp1 + frp2;
+		ar2 = secp1 + secp2;
+		if (ar2 > 99)
+		{
+			ar1 += 1;
+			ar2 = ar2 % 100;
+			if (ar2 < 10)
+				printf("%lu0%d", ar1, ar2);
+		}
+		if (ar2 >= 10)
+			printf("%lu%d", ar1, ar2);
+		if (b < 6)
 			printf(", ");
-		first = second;
-		second = f;
+		frp1 = frp2;
+		frp2 = ar1;
+		secp1 = secp2;
+		secp2 = ar2;
 	}
 	printf("\n");
 	return (0);
