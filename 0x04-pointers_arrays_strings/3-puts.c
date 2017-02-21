b0caf6982c3042199408ab6e7d4d680f09eebf59
Christian Agha <christianagha@gmail.com>
2017-02-21 19:56:30 +0000

    added 3-puts.c

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: input string
+ * Return: void
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar (str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
