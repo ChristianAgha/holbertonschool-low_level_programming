0a7bcfbcfa6d57cb026a78be7d7dad3abff02d8f
Christian Agha <christianagha@gmail.com>
2017-02-21 23:42:48 +0000

    updated 5-rev_string.c

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,13 @@
  */
 void rev_string(char *s)
 {
-	unsigned int j = 0, i = 0, l = 0;
+	unsigned int j = 0, i = 0;
 	char c[1000];
 
 	while (s[i] != '\0')
 	{
+		c[i] = s[i];
 		i++;
-	}
-	for (l = 0; l <= i; l++)
-	{
-		c[l] = s[l];
 	}
 	while (i > 0)
 	{
