bf9daf1dc89cee689029d51c632159851f0db6bd
Christian Agha <christianagha@gmail.com>
2017-02-21 23:21:49 +0000

    added 5-rev_string.c

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * rev_string - a function that reverses a string
+ * @s: input string
+ * Return: Void
+ *
+ */
+void rev_string(char *s)
+{
+	int j = 0, i = 0, l = 0;
+	char c[1000];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (l = 0; l <= i; l++)
+	{
+		c[l] = s[l];
+	}
+	while (i >= 0)
+	{
+		s[j] = c[i-1];
+		i--;
+		j++;
+	}
+}
