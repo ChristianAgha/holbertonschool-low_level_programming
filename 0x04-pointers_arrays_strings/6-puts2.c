8d9d8c7f60e12d227bbc5cefaeb87961dd08efe5
Christian Agha <christianagha@gmail.com>
2017-02-22 00:01:52 +0000

    added 6-puts2.c

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: input string
+ * Return: Void
+ *
+ */
+void puts2(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
