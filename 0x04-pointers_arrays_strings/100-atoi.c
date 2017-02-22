89ab7764cf507133a604a26822df74580735c104
Christian Agha <christianagha@gmail.com>
2017-02-22 07:30:22 +0000

    changed to subtract instead of adding to protect againts overflow

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int minus = 1;
+	int minus = -1;
 	int res = 0;
 	int isnum = 0;
 
@@ -18,7 +18,7 @@ int _atoi(char *s)
 			minus *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			res = res * 10 + (s[i] - '0');
+			res = res * 10 - (s[i] - '0');
 			isnum = 1;
 		}
 		else if (isnum == 1)
