09c971f30caafd62b08293e65558ec33e2aaa62a
Christian Agha <christianagha@gmail.com>
2017-02-23 21:36:13 +0000

    update 3-strcmp.c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,8 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return ((s2[i] - '0') - (s1[i] - '0'));
+			return (s2[i] - s1[i]);
 		i++;
 	}
-	return (0);
+	return (s2[i] - s1[i]);
 }
