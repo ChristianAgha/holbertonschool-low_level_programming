31c5804d86da6b09a6e6916edb44f4a0b9e48fcc
Christian Agha <christianagha@gmail.com>
2017-02-24 03:17:56 +0000

    added 8-rot13.c

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @c: input string
+ * Return: modified input string
+ *
+ */
+char *rot13(char *c)
+{
+	int i;
+	int j;
+	char leet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char l[] =    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+			if (c[i] == l[j])
+			{
+				c[i] = leet[j];
+				break;
+			}
+	}
+	return (c);
+}
