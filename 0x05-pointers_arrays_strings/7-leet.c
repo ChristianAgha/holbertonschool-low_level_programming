b263a42b4c80f283b49bc9a200198cae33d91ffd
Christian Agha <christianagha@gmail.com>
2017-02-24 01:56:19 +0000

    added 7-leet.c

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @c: input string
+ * Return: modified input string
+ *
+ */
+char *leet(char *c)
+{
+	int i;
+	int j;
+	char leet[] = "4433007711";
+	char l[] = "aAeEoOtTlL";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (c[i] == l[j])
+			    c[i] = leet[j];
+		}
+	}
+       	return (c);
+
+}
