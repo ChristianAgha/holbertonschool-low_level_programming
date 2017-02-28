299e57dfc105c13b19d63e8d9a8a2fb57ccdd0c7
Christian Agha <christianagha@gmail.com>
2017-02-28 07:06:37 +0000

    added 8-print_diagsums.c

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums -  a function that prints the sum of the two matrixs
+ * @a: pointer to matrix
+ * @size: size of matrix
+ * Return: void
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int a_res = 0;
+
+	while (i < (size * size))
+	{
+		a_res += a[i];
+		i += size + 1;
+	}
+	printf("%d, ", a_res);
+	a_res = 0;
+	i = size - 1;
+	a_res = a[i];
+	while (i < ((size * size) - size))
+	{
+		i += size - 1;
+		a_res += a[i];
+	}
+	printf("%d\n", a_res);
+}
