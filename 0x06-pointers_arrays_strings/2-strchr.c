34f2c9b526e011abff135fd9e90a1a9583581392
Christian Agha <christianagha@gmail.com>
2017-02-28 01:13:28 +0000

    updated 2-strchr.c

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; i < (sizeof(s) / sizeof(char)); i++)
+	for (i = 0; i <= (sizeof(s) / sizeof(char)); i++)
 	{
 		if (s[i] == c)
 		{
