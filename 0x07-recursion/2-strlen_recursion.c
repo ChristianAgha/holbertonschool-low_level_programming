ef71919a45217351da1247817130706bce3af56d
Christian Agha <christianagha@gmail.com>
2017-03-02 00:53:53 +0000

    added 2-strlen_recursion.c

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
 	s++;
-       	return (_strlen_recursion(s) + 1);
+	if (s[-1] != '\0')
+	{
+		return (_strlen_recursion(s) + 1);
+	}
+	return (0);
 }
