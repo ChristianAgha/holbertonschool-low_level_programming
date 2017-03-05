f03f516aee3ab4478ec2748477db889640b104f6
Christian Agha <christianagha@gmail.com>
2017-03-05 18:58:38 +0000

    added 0-whatsmyname.c

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments passed to program
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
