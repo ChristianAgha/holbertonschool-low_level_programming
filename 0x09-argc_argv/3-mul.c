0136e9bfa1f6ac06ed843253e686e4eaeb6c1072
Christian Agha <christianagha@gmail.com>
2017-03-05 19:25:15 +0000

    added 3-mul.c

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments passed to program
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
+
+	return (0);
+}
