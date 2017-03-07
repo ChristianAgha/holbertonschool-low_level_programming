51909b43c60f4185681fc4c9aa6610d982247cfd
Christian Agha <christianagha@gmail.com>
2017-03-07 23:31:52 +0000

    added 1-strdup.c

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate
+ * of the string str
+ * @str: input string
+ *
+ * Return: a pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, len;
+	char *string;
+
+	len = 0;
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len++;
+
+	string = malloc(sizeof(char) * len);
+
+	if (string == NULL)
+	{
+		free(string);
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		string[i] = str[i];
+	}
+	string[i] = '\0';
+	return (string);
+}
