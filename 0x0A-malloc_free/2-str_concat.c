2777e3f25b45c6f1f984c8da88903e9c31fa4801
Christian Agha <christianagha@gmail.com>
2017-03-08 15:46:46 +0000

    modified loops and type of malloc

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -19,13 +19,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	while (s1[len1] != '\0')
+		len1++;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	while (s2[len2] != '\0')
+		len2++;
 
+	s3 =  (char *) malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 	{
 		free(s3);
