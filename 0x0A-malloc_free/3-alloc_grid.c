c42090e0d66afec806a69820dec238f13fee03d2
Christian Agha <christianagha@gmail.com>
2017-03-08 15:55:23 +0000

    added type cast to malloc

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -10,32 +10,32 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array, i, j;
+	int **array2d, i, j;
 
 	if ((width < 1) || (height < 1))
 		return (NULL);
 
-	array = (int **)malloc(sizeof(array) * height);
-	if (array == NULL)
+	array2d = (int **) malloc(sizeof(array2d) * height);
+	if (array2d == NULL)
 	{
-		free(array);
+		free(array2d);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(sizeof(int) * width);
-		if (array[i] == NULL)
+		array2d[i] = (int *) malloc(sizeof(int) * width);
+		if (array2d[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
-				free(array[j]);
-			free(array);
+				free(array2d[j]);
+			free(array2d);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 		{
-			array[i][j] = 0;
+			array2d[i][j] = 0;
 		}
 	}
 
-	return (array);
+	return (array2d);
 }
