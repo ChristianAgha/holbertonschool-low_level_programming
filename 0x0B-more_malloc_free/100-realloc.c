5ced47d5729da0592cbd40d896ca59ce7a0a4e4d
Christian Agha <christianagha@gmail.com>
2017-03-10 05:12:13 +0000

    added free (ptr)

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -36,5 +36,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	for (i = 0; i < old_size && i < new_size; i++)
 		newptr[i] = ((char *)ptr)[i];
 
+	free(ptr);
 	return (newptr);
 }
