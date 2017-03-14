0ce92e8ed45953cd9fd87629b302b4a3a8bbb0e2
Christian Agha <christianagha@gmail.com>
2017-03-14 01:47:12 +0000

    updated dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- * struct dog_t - dog info
+ * struct dog_t - struct for dog info
+ * dog_t - typedef for dog
  * @name: name of the dog
  * @age: dog's age
  * @owner: dog's owner
  *
+ * Description: a struct that creates a dog instance
  */
 typedef struct dog {
 	char *name;
