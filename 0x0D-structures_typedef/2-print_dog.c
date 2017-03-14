5a54cdc0d47d2c386e485c704dc89ac0903298db
Christian Agha <christianagha@gmail.com>
2017-03-14 01:26:25 +0000

    fixed betty mistake

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Naaaame: (nil)\n");
-	       	printf("Age: %f\n", d->age);
-	       	if (d->owner)
+		printf("Age: %f\n", d->age);
+		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
