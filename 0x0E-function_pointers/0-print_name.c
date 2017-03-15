c3aaf659147259c23b11b8ae57ecfa3eb2acbf22
Christian Agha <christianagha@gmail.com>
2017-03-15 15:51:51 +0000

    edited comments and removed ()

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -3,9 +3,8 @@
  * print_name - prints a name
  * @name: input string
  * @f: pointer to function
- * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	f(name);
 }
