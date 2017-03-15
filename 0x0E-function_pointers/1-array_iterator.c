808841462d054f2d3d8f3c69b7f4bd147c14f462
Christian Agha <christianagha@gmail.com>
2017-03-15 16:31:13 +0000

    removed include

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include "function_pointers.h"
-
 /**
  * array_iterator - executes a function given as a parameter
  * on each element of an array
@@ -10,7 +8,6 @@
  *
  * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
