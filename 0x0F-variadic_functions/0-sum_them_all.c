170dfcd56e71e6bcba783b5c1a79cced198629cb
Christian Agha <christianagha@gmail.com>
2017-03-18 19:57:07 +0000

    removed if

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -12,17 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	sum = 0;
-	if(n)
+	if (n == 0)
+		return (0);
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-			return (0);
-		va_start(valist, n);
-
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(valist, int);
-		}
-		va_end(valist);
+		sum += va_arg(valist, int);
 	}
+	va_end(valist);
 	return (sum);
 }
