ff864f683135d797aa5b1860c8cc85782eded636
Christian Agha <christianagha@gmail.com>
2017-03-18 20:10:19 +0000

    changed typedef comment

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,18 +1,18 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 /**
  * struct type - struct for input
  * arg_t - typedef for type
- * @c: input char
- * @s: input string
+ * @type: input char
+ * @f: input string
  */
 typedef struct type
 {
 	char *type;
 	void (*f)();
 } arg_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
