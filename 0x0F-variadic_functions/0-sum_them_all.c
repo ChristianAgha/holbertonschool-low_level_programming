3dfbcf2309385f0e5cca83cd7df0efc43781bf1f
Christian Agha <christianagha@gmail.com>
2017-03-18 19:59:56 +0000

    added an include

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * sum_them_all - a function that sums all its input arguments
  * @n: the number of arguments being input
