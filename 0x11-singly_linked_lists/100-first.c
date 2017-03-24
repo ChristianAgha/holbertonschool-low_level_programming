2dd69b0e1af4c3d968fd541405dace956747f8e7
Christian Agha <christianagha@gmail.com>
2017-03-24 06:12:13 +0000

    added 100-first.c

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * pre_main - a function that excuted before the main function
+ * Return: the length of the string
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
