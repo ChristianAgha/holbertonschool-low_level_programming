8865c35c293bd08c1040413d14a66b656e9d9cd6
Christian Agha <christianagha@gmail.com>
2017-03-30 22:22:07 +0000

    fixed comments

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- * print_binary - prints the binary representation of a number
- * @n: integer to be converted to a binary string
- * Return: void
+ * get_bit -  returns the value of a bit at a given index.
+ * @n: input integer
+ * @index: the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
