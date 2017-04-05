937d1575373f8557ea5e3cf518192d6b36adff1d
Christian Agha <christianagha@gmail.com>
2017-04-05 16:27:56 +0000

    reverted to older version

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -7,32 +7,39 @@
  **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int text_file, reed, rite;
+	int text_file;
 	char *buffer;
+	ssize_t reed, rite;
 
 	if (filename == NULL)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
 	text_file = open(filename, O_RDONLY);
 	if (text_file == -1)
-		free(buffer); return (0);
-
+	{
+		free(buffer);
+		return (0);
+	}
 	reed = read(text_file, buffer, letters);
 	if (reed == -1)
-		free(buffer); return (0);
-
+	{
+		free(buffer);
+		return (0);
+	}
 	rite = write(STDOUT_FILENO, buffer, reed);
 	if (rite == -1)
-		free(buffer); return (0);
-
+	{
+		free(buffer);
+		return (0);
+	}
 	text_file  = close(text_file);
 	if (text_file == -1)
-		free(buffer); return (0);
-
+	{
+		free(buffer);
+		return (0);
+	}
 	free(buffer);
-
 	return (reed);
 }
