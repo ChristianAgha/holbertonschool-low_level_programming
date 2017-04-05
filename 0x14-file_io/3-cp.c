64315b8185bf11c7127f559980c88604e21f6d87
Christian Agha <christianagha@gmail.com>
2017-04-05 08:04:28 +0000

    chenaged rite buffer to reed

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -7,9 +7,10 @@
  */
 int main(int argc, char **argv)
 {
-	int file_from, file_to, reed, rite, length;
+	int file_from, file_to, reed, rite;
 	char buffer[1204];
 
+	reed = 1;
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
@@ -21,17 +22,21 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	reed = read(file_from, buffer, 1204);
-	if (reed == -1)
+	while (reed)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		reed = read(file_from, buffer, 1204);
+		if (reed == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		rite = write(file_to, buffer, reed);
+		if (rite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	for (length = 0; buffer[length] != '\0'; length++)
-		;
-	rite = write(file_to, buffer, length);
-	if (rite == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd  %s\n", argv[1]), exit(100);
 	if (close(file_to) == -1)
