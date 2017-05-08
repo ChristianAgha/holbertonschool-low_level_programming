35788c802c45cc838cd25985fb9640759c1dfd70
Christian Agha <christianagha@gmail.com>
2017-05-08 06:49:05 +0000

    changed the location of setting prev to NULL

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,9 +15,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = NULL;
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 		return (*head);
 	}
