4479eca63219e691a1b7bee38d55086769957d20
Christian Agha <christianagha@gmail.com>
2017-05-10 21:19:21 +0000

    added ability to insert node at end of list

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,38 +10,60 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *b4_new_node;
 	unsigned int i;
+	size_t list_length;
 
 	i = 0;
+	list_length = dlistint_len(*h);
 	b4_new_node = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+	new_node->next = new_node->prev = NULL;
+	if (list_length < idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	if (idx == 0)
 	{
-		new_node->next = *h;
 		if (*h != NULL)
+		{
+			new_node->next = *h;
 			(*h)->prev = new_node;
+		}
 		*h = new_node;
 		return (*h);
 	}
 	while (i < idx - 1)
 	{
 		b4_new_node = b4_new_node->next;
-		if (b4_new_node->next == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
 		i++;
 	}
 	new_node->next = b4_new_node->next;
 	new_node->prev = b4_new_node;
 	b4_new_node->next = new_node;
-	new_node->next->prev = new_node;
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
 
 	return (new_node);
+}
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to doubly linked list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int num_node;
+
+	num_node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num_node++;
+	}
+	return (num_node);
 }
