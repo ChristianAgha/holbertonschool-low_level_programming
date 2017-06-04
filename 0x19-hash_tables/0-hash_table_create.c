8dc3c458330d59bf61a007ea8839ab832e97cf23
Christian Agha <christianagha@gmail.com>
2017-06-04 23:13:39 +0000

    initial commit

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create -  creates a hash table
+ * @size: size of hash table to be created
+ * Return: pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *Hashsicle;
+
+	if (size <= 0)
+		return (NULL);
+
+	Hashsicle = malloc(sizeof(hash_table_t));
+	if (Hashsicle == NULL)
+		return (NULL);
+
+	Hashsicle->size = size;
+
+	Hashsicle->array = calloc(size, sizeof(hash_node_t));
+	if (Hashsicle->array == NULL)
+	{
+		free(Hashsicle);
+		return (NULL);
+	}
+
+	return (Hashsicle);
+}
