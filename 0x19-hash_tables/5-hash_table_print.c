e8b586231b72637df845ea97505c3ec42a868788
Christian Agha <christianagha@gmail.com>
2017-06-05 09:00:02 +0000

    changed and shortened logic

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -14,14 +14,10 @@ int count_hash_table(const hash_table_t *ht)
 	for (i = 0; i < BUFSIZE; i++)
 	{
 		tmp = ht->array[i];
-		if (tmp != NULL)
+		while (tmp != NULL)
 		{
 			items++;
-			while (tmp->next != NULL)
-			{
-				items++;
-				tmp = tmp->next;
-			}
+			tmp = tmp->next;
 		}
 	}
 	return (items);
@@ -33,37 +29,25 @@ int count_hash_table(const hash_table_t *ht)
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long i, items, size;
+	unsigned long i, items;
 	hash_node_t *tmp;
 
 	if (ht == NULL || ht->array == NULL)
 		return;
 
 	items = count_hash_table(ht);
-	size = ht->size;
 
 	printf("{");
-	for (i = 0; i < size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
-		if (tmp != NULL)
+		while (tmp != NULL)
 		{
 			printf("'%s': '%s'", tmp->key, tmp->value);
 			if (items > 1)
-			{
 				printf(", ");
-				items--;
-			}
-			while (tmp->next != NULL)
-			{
-				tmp = tmp->next;
-				printf("'%s': '%s'", tmp->key, tmp->value);
-				if (items > 1)
-				{
-					printf(", ");
-					items--;
-				}
-			}
+			items--;
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
