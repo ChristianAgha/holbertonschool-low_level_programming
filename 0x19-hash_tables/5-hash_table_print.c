88705667acfd024c8a801ecf96156e48311ff4c5
Christian Agha <christianagha@gmail.com>
2017-06-05 09:15:13 +0000

    revert

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -38,7 +38,7 @@ void hash_table_print(const hash_table_t *ht)
 	items = count_hash_table(ht);
 
 	printf("{");
-	for (i = 0; i <= ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
 		while (tmp != NULL)
