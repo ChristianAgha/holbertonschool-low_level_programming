349e5f9ed0e2884f228441629def3e5495388b5e
Christian Agha <christianagha@gmail.com>
2017-06-05 04:43:14 +0000

    added check for null input value

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *Ptr, *tmp;
 
 	node = malloc(sizeof(hash_node_t));
-	if (node == NULL || ht == NULL || key == NULL)
+	if (node == NULL || ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
