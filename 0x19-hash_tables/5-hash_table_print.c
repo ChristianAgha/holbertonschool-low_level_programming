162f74edeaf4a3f68c56c38135170f90410393c6
Christian Agha <christianagha@gmail.com>
2017-06-05 08:23:17 +0000

    adding prob #5

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,69 @@
+#include "hash_tables.h"
+/**
+ * count_hash_table- coints the items in the hash table
+ * @ht: hash table
+ * Return: number of items
+ */
+int count_hash_table(const hash_table_t *ht)
+{
+	int i, items;
+	hash_node_t *tmp;
+
+	items = 0;
+
+	for (i = 0; i < BUFSIZE; i++)
+	{
+		tmp = ht->array[i];
+		if (tmp != NULL)
+		{
+			items++;
+			while (tmp->next != NULL)
+			{
+				items++;
+				tmp = tmp->next;
+			}
+		}
+	}
+	return (items);
+}
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	int i, items;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return;
+
+	items = count_hash_table(ht);
+
+	printf("{");
+	for (i = 0; i < BUFSIZE; i++)
+	{
+		tmp = ht->array[i];
+		if (tmp != NULL)
+		{
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (items > 1)
+			{
+				printf(", ");
+				items--;
+			}
+			while (tmp->next != NULL)
+			{
+				tmp = tmp->next;
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				if (items > 1)
+				{
+					printf(", ");
+					items--;
+				}
+			}
+		}
+	}
+	printf("}\n");
+}
