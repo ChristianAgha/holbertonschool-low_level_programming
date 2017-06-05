419e049356e74564cafaf02293acd70399edeaa5
Christian Agha <christianagha@gmail.com>
2017-06-05 04:52:37 +0000

    added null value check

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *Ptr, *tmp;
 
 	node = malloc(sizeof(hash_node_t));
-	if (!node || !ht || !key || strlen(key) == 0)
+	if (!node || !ht || !key || strlen(key) == 0 || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
