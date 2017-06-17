7cb21c3276543f53a79a78f2d524529f55de639d
Christian Agha <christianagha@gmail.com>
2017-06-16 17:04:23 -0700

    added check for size being <= 0

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -24,6 +24,9 @@ void bubble_sort(int *array, size_t size)
 
 	if (!array)
 		return;
+	if (size <= 0)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < (size - i - 1); j++)
 			if (array[j] > array[j + 1])
