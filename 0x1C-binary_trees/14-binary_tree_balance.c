6f2363c941023d5b334699d85fcbb19cfabf1397
Christian Agha <christianagha@gmail.com>
2017-07-27 15:05:36 -0700

    removed function and added an include in its place

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,25 +1,5 @@
 #include "binary_trees.h"
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: tree root node
- * Return: height of the tree
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	int left_height;
-	int right_height;
-
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
-
-	if (left_height > right_height)
-		return (left_height);
-	else
-		return (right_height);
-}
+#include "9-binary_tree_height.c"
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree
