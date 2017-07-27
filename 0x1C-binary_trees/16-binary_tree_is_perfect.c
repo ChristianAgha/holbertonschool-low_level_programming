88a29b745ef6866a5e8a243bac56261b32d3235d
Christian Agha <christianagha@gmail.com>
2017-07-27 14:54:12 -0700

    adding prob #16

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+#include "11-binary_tree_size.c"
+#include "14-binary_tree_balance.c"
+#include "15-binary_tree_is_full.c"
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: tree root node
@@ -6,11 +9,16 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	
+	if (binary_tree_balance(tree) == 0)
+		if (binary_tree_is_full(tree) == 1)
+			if ((binary_tree_size(tree) % 2) != 0)
+				return (1);
+
+	return (0);
 }
