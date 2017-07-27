337cb4f2900832683bbe13d8f0bcd1f02ea11a21
Christian Agha <christianagha@gmail.com>
2017-07-27 16:01:38 -0700

    changed it completely

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,7 +1,24 @@
 #include "binary_trees.h"
 #include "11-binary_tree_size.c"
-#include "14-binary_tree_balance.c"
-#include "15-binary_tree_is_full.c"
+#include "9-binary_tree_height.c"
+/**
+ * power - gets power result
+ * @base: base number
+ * @exponent: exponent
+ * Return: result
+ */
+unsigned int power(int base, int exponent)
+{
+	unsigned int result = 1;
+
+	while (exponent != 0)
+	{
+		result *= base;
+		--exponent;
+	}
+
+	return (result);
+}
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: tree root node
@@ -9,16 +26,15 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	unsigned int height;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+	height = binary_tree_height(tree);
 
-	if (binary_tree_balance(tree) == 0)
-		if (binary_tree_is_full(tree) == 1)
-			if ((binary_tree_size(tree) % 2) != 0)
-				return (1);
+	if (binary_tree_size(tree) == power(2, height + 1) - 1)
+		return (1);
 
 	return (0);
 }
